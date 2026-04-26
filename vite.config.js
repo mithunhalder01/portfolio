911aa9102f7ac1576f78e9704f6d78bde31f7a83
Mithun Halder <mithunhaldar7500@gmail.com>
2026-04-26 20:29:57 +0530

    sdfb

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mithunhalder01.github.io/' // Updated base to match GitHub Pages repository
+  base: '/portfolio/' // Updated base to match GitHub Pages repository
 })
